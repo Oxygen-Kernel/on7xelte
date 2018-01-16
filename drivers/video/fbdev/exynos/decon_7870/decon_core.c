5883e2fd4ae43689339dff42fd0e34db4e872c2d
arter97 <qkrwngud825@gmail.com>
2018-01-16 03:24:55 +0100

    decon: add powersuspend hooks
    Signed-off-by: morogoku <morogoku@hotmail.com>
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/video/fbdev/exynos/decon_7870/decon_core.c
+++ drivers/video/fbdev/exynos/decon_7870/decon_core.c
@@ -42,6 +42,10 @@
 #include <linux/state_notifier.h>
 #endif
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #include "decon.h"
 #include "dsim.h"
 #include "decon_helper.h"
@@ -1570,6 +1574,9 @@ static int decon_blank(int blank_mode, struct fb_info *info)
 	case FB_BLANK_NORMAL:
 		DISP_SS_EVENT_LOG(DISP_EVT_BLANK, &decon->sd, ktime_set(0, 0));
 		ret = decon_disable(decon);
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 		if (ret) {
 			decon_err("failed to disable decon\n");
 			goto blank_exit;
@@ -1581,6 +1588,9 @@ static int decon_blank(int blank_mode, struct fb_info *info)
 	case FB_BLANK_UNBLANK:
 		DISP_SS_EVENT_LOG(DISP_EVT_UNBLANK, &decon->sd, ktime_set(0, 0));
 		ret = decon_enable(decon);
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
 		if (ret) {
 			decon_err("failed to enable decon\n");
 			goto blank_exit;
