efaa02cfb3dc4301daf0ec7b5992a09e47715f34
djb77 <dwayne.bakewell@gmail.com>
2018-02-05 00:58:54 +1100

    cpufreq: wheatley Governor
    Signed-off-by: morogoku <morogoku@hotmail.com>
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -571,6 +571,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_USERSPACE)
 extern struct cpufreq_governor cpufreq_gov_userspace;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_userspace)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wheatley)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmoove)
