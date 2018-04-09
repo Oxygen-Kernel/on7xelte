fd348b27b0bbbccc16ff7df00978262d79b4dd53
Abhay Ruparel <abhayruparel2000@gmail.com>
2018-04-09 03:42:48 +0000

    Linux: 3.18.103

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -472,14 +472,6 @@ static inline unsigned long vfs_pressure_ratio(unsigned long val)
 {
 	return mult_frac(val, sysctl_vfs_cache_pressure, 100);
 }
-
-struct name_snapshot {
-	const char *name;
-	char inline_name[DNAME_INLINE_LEN];
-};
-void take_dentry_name_snapshot(struct name_snapshot *, struct dentry *);
-void release_dentry_name_snapshot(struct name_snapshot *);
-
 /**
  * d_inode - Get the actual inode of this dentry
  * @dentry: The dentry to query
